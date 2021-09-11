8615d09ef250fd3b9f6a2e0b39b767860f5c6ce2
Purnachand <Purnachand_Jaddu@mentor.com>
2021-09-11 18:08:13 +0530

    Implemented Hashset using go

---
+++ HashSet.go
@@ -0,0 +1,41 @@
+package main
+
+import "fmt"
+
+type HashSet struct {
+	valMap map[interface{}]bool
+}
+
+func (s *HashSet) add(key interface{}) {
+	s.valMap[key] = true
+}
+
+func (s *HashSet) remove(key interface{}) {
+	delete(s.valMap, key)
+}
+
+func (s *HashSet) contains(key interface{}) bool {
+	return s.valMap[key]
+}
+
+func (s *HashSet) size() int {
+	return len(s.valMap)
+}
+
+func main() {
+	set := HashSet{}
+	set.valMap = map[interface{}]bool{}
+	set.add(1)
+	set.add(2)
+	set.add(3)
+
+	set.remove(4)
+	fmt.Printf(" Size of set is %d \n", set.size())
+	set.remove(3)
+	fmt.Printf(" Size of set is %d \n", set.size())
+	set.add(5)
+	fmt.Printf(" Size of set is %d \n", set.size())
+	fmt.Println(set.contains(1))
+	fmt.Println(set.contains(100))
+
+}
